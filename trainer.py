ef35e0502b03b7c27848c67c531f6139290ed166
mel-moun <mrymgithub@gmail.com>
2025-01-14 16:34:24 +0100

    prog1

--- trainer.py
+++ trainer.py
@@ -1,15 +1,22 @@
 def read_thetas():
-    with open("thetas.txt", "r") as data:
-        thetas = data.read().strip().split(',')
-        thetas[0] = float(thetas[0])
-        thetas[1] = float(thetas[1])
-    return thetas
+    try:
+        with open("thetas.txt", "r") as data:
+            thetas = data.read().strip().split(',')
+            thetas = [float(x) for x in thetas]
+        return thetas
+    except BaseException:
+        return [0, 0]
+
+
+def estimate_price(mileage, thetas):
+    return thetas[0] + (thetas[1] * mileage)
+
 
 def main():
     try:
         mileage = float(input("Enter a mileage: "))
-
-        read_thetas()
+        thetas = read_thetas()
+        print("Estimated price:", estimate_price(mileage, thetas))
     except BaseException as e:
         print(type(e).__name__, ":", e)
 
